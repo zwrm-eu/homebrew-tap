90502d01fa4efd0ae0a8edcb6d180328dc03f422
Tom Jordi Ruesch <tomjordi@zwrm.eu>
2026-03-10 07:57:50 +0100

    Initial formula (placeholder)

---
+++ Formula/zwrm.rb
@@ -0,0 +1,46 @@
+class Zwrm < Formula
+  desc "CLI for deploying and managing microVMs on ZWRM"
+  homepage "https://github.com/zwrm-eu/zwrm"
+  version "0.0.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://releases.zwrm.eu/zwrmd/v0.0.0/zwrm-darwin-arm64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "zwrm-darwin-arm64" => "zwrm"
+      end
+    elsif Hardware::CPU.intel?
+      url "https://releases.zwrm.eu/zwrmd/v0.0.0/zwrm-darwin-amd64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "zwrm-darwin-amd64" => "zwrm"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://releases.zwrm.eu/zwrmd/v0.0.0/zwrm-linux-arm64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "zwrm-linux-arm64" => "zwrm"
+      end
+    elsif Hardware::CPU.intel?
+      url "https://releases.zwrm.eu/zwrmd/v0.0.0/zwrm-linux-amd64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "zwrm-linux-amd64" => "zwrm"
+      end
+    end
+  end
+
+  test do
+    assert_match "zwrm", shell_output("#{bin}/zwrm --help")
+  end
+end
