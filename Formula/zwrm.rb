b9bb5fd4f163f97f290b8f1987b0bbc51b5e7859
Tom Jordi Ruesch <tomjordi@zwrm.eu>
2026-04-05 17:11:11 +0000

    zwrm v0.7.5

--- Formula/zwrm.rb
+++ Formula/zwrm.rb
@@ -1,20 +1,20 @@
 class Zwrm < Formula
   desc "CLI for deploying and managing microVMs on ZWRM"
   homepage "https://github.com/zwrm-eu/zwrm"
-  version "0.7.4"
+  version "0.7.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://releases.zwrm.eu/zwrmd/v0.7.4/zwrm-darwin-arm64"
-      sha256 "5fd0e913d4c45901fa63b96439502f19396a0f9d1b07b5327953c9fab28c9980"
+      url "https://releases.zwrm.eu/zwrmd/v0.7.5/zwrm-darwin-arm64"
+      sha256 "18adb3195775a0aa52e66a5688f2a02cab099919f683a10cbd890bf09c28f62b"
 
       def install
         bin.install "zwrm-darwin-arm64" => "zwrm"
       end
     elsif Hardware::CPU.intel?
-      url "https://releases.zwrm.eu/zwrmd/v0.7.4/zwrm-darwin-amd64"
-      sha256 "64d6c77857336eef467c813e5acfe46dbc5c66a4fe556185a5f1b8eea08dec5f"
+      url "https://releases.zwrm.eu/zwrmd/v0.7.5/zwrm-darwin-amd64"
+      sha256 "dbc425a478ef5ce59f0a7519abda854247e6b073323c6ba70d6ac52ace6c2976"
 
       def install
         bin.install "zwrm-darwin-amd64" => "zwrm"
@@ -24,15 +24,15 @@ class Zwrm < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://releases.zwrm.eu/zwrmd/v0.7.4/zwrm-linux-arm64"
-      sha256 "72672d5cf5d9a909d3017ca3df9c6f28c2e68f4686c04ec2071de1de452c3854"
+      url "https://releases.zwrm.eu/zwrmd/v0.7.5/zwrm-linux-arm64"
+      sha256 "966bd5ce87801e50a7eaf3c0c6065a9021f6b941f880ff289e87ae8655129dd8"
 
       def install
         bin.install "zwrm-linux-arm64" => "zwrm"
       end
     elsif Hardware::CPU.intel?
-      url "https://releases.zwrm.eu/zwrmd/v0.7.4/zwrm-linux-amd64"
-      sha256 "f0fd7e8265162d0c44e74b78595f6cb8d7aa86a8805b3b6762b64421efa244c0"
+      url "https://releases.zwrm.eu/zwrmd/v0.7.5/zwrm-linux-amd64"
+      sha256 "b1e9645a7cdb88252ef566b0da549215a362ade0d0fc73bc2559e6f863ed88e2"
 
       def install
         bin.install "zwrm-linux-amd64" => "zwrm"
