3653ed969524a5c600ff918d6947966235b476da
Tom Jordi Ruesch <tomjordi@zwrm.eu>
2026-08-03 19:15:36 +0000

    zwrm v0.22.1

--- Formula/zwrm.rb
+++ Formula/zwrm.rb
@@ -1,20 +1,20 @@
 class Zwrm < Formula
   desc "CLI for deploying and managing microVMs on ZWRM"
   homepage "https://github.com/zwrm-eu/zwrm"
-  version "0.22.0"
+  version "0.22.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://releases.zwrm.eu/zwrmd/v0.22.0/zwrm-darwin-arm64"
-      sha256 "96dd13f4afbcd811ecb73187f1b6e07e119a4b7bb80b1fa5d5465f1f0b1331f8"
+      url "https://releases.zwrm.eu/zwrmd/v0.22.1/zwrm-darwin-arm64"
+      sha256 "d649d04ddc97de3d75a9e757cbcaf31d1e16840339bf5dc9cd27e18579859453"
 
       def install
         bin.install "zwrm-darwin-arm64" => "zwrm"
       end
     elsif Hardware::CPU.intel?
-      url "https://releases.zwrm.eu/zwrmd/v0.22.0/zwrm-darwin-amd64"
-      sha256 "c1f0d124a565dc59be308c727f890c6ad16ea296874a3910ded4a8227a2003d0"
+      url "https://releases.zwrm.eu/zwrmd/v0.22.1/zwrm-darwin-amd64"
+      sha256 "f7273d4d34fa1da60808ba0f665f274cc0abfc47a5bd36a384b0209efefc8d81"
 
       def install
         bin.install "zwrm-darwin-amd64" => "zwrm"
@@ -24,15 +24,15 @@ class Zwrm < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://releases.zwrm.eu/zwrmd/v0.22.0/zwrm-linux-arm64"
-      sha256 "77055403165e9f3e4edbcb56615f74daad8d330fe6ae37839a95e81028944a39"
+      url "https://releases.zwrm.eu/zwrmd/v0.22.1/zwrm-linux-arm64"
+      sha256 "004277bda551ad4bb0cb71c61e293659d87652385bd4f9331f61cd7b4fc07e9b"
 
       def install
         bin.install "zwrm-linux-arm64" => "zwrm"
       end
     elsif Hardware::CPU.intel?
-      url "https://releases.zwrm.eu/zwrmd/v0.22.0/zwrm-linux-amd64"
-      sha256 "4d30f713c1ded172740f4921e79a72824e58c063cbcbfdcf3f194b9a5ed8eb40"
+      url "https://releases.zwrm.eu/zwrmd/v0.22.1/zwrm-linux-amd64"
+      sha256 "56500d173a7d94b8e4c6d2fe87b6b67466bceb3b4a433ef89cf25ce353600444"
 
       def install
         bin.install "zwrm-linux-amd64" => "zwrm"
