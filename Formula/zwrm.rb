96fdfc753ff518b03eec9bb9b91efae3be41c407
Tom Jordi Ruesch <tomjordi@zwrm.eu>
2026-08-30 07:42:08 +0000

    zwrm v0.24.11

--- Formula/zwrm.rb
+++ Formula/zwrm.rb
@@ -1,20 +1,20 @@
 class Zwrm < Formula
   desc "CLI for deploying and managing microVMs on ZWRM"
   homepage "https://github.com/zwrm-eu/zwrm"
-  version "0.24.10"
+  version "0.24.11"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://releases.zwrm.eu/zwrmd/v0.24.10/zwrm-darwin-arm64"
-      sha256 "2144c3104e357941a599ea5c86ec6fb4cf4511bf406333ca5450061e5101f02d"
+      url "https://releases.zwrm.eu/zwrmd/v0.24.11/zwrm-darwin-arm64"
+      sha256 "3838d32db705c2fe8059a1f2b71254e750458e3d830b36501275a765fa13ead0"
 
       def install
         bin.install "zwrm-darwin-arm64" => "zwrm"
       end
     elsif Hardware::CPU.intel?
-      url "https://releases.zwrm.eu/zwrmd/v0.24.10/zwrm-darwin-amd64"
-      sha256 "3708006989c03792081256537bf0b1685734da0541220ee7cf45eed535e9c83a"
+      url "https://releases.zwrm.eu/zwrmd/v0.24.11/zwrm-darwin-amd64"
+      sha256 "a06b0d88ac463d08e584072b49372a63eb4e0ca82b202dab9b8b18daa5475c19"
 
       def install
         bin.install "zwrm-darwin-amd64" => "zwrm"
@@ -24,15 +24,15 @@ class Zwrm < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://releases.zwrm.eu/zwrmd/v0.24.10/zwrm-linux-arm64"
-      sha256 "2c1a51be0a5f582b5d3983e88b0bb10de611ae42f95b5d28d7921b30e4fab190"
+      url "https://releases.zwrm.eu/zwrmd/v0.24.11/zwrm-linux-arm64"
+      sha256 "2364c432448618f56ea3e4ea52254318a85a655dc40fd5ec416c08c747c209bc"
 
       def install
         bin.install "zwrm-linux-arm64" => "zwrm"
       end
     elsif Hardware::CPU.intel?
-      url "https://releases.zwrm.eu/zwrmd/v0.24.10/zwrm-linux-amd64"
-      sha256 "4d98ea15af1ea39cd75c4a2f52130d6929a88e4e16e37ca7237f3685ef6a72dd"
+      url "https://releases.zwrm.eu/zwrmd/v0.24.11/zwrm-linux-amd64"
+      sha256 "68ebec7201392515cc4e82fac7eb4cd25e3d20b6957dc462e3a31f00cc7e4460"
 
       def install
         bin.install "zwrm-linux-amd64" => "zwrm"
